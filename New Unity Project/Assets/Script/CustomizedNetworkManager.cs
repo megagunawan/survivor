e6366b035afc2954c45f018691f9fb521c98cadb
seeker-Liu <possnioo2@gmail.com>
2018-05-12 14:17:24 +0800

    network

---
+++ New Unity Project/Assets/Script/CustomizedNetworkManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class CustomizedNetworkManager : NetworkManager {
+	
+	public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
+	{
+		System.Random myRandom = new System.Random();
+		Vector3 playerPos ;
+		playerPos.x = -200f + 400f * (float) myRandom.NextDouble ();
+		playerPos.y = 4f;
+		playerPos.z = -200f + 400f * (float) myRandom.NextDouble ();
+		GameObject player = (GameObject)Instantiate(playerPrefab, playerPos, Quaternion.identity);
+		NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
+	}
+}
